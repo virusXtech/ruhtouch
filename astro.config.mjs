56a31b6b2416685dfcd310cf8190f52aab21642b
Md Faisal Alam <thisisfaisalhere@gmail.com>
2025-09-27 18:35:00 +0530

    feat: netlify deploy ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
 	output: 'server',
-	adapter: node({
-		mode: 'standalone'
-	}),
+	adapter: netlify(),
 	integrations: [tailwind()],
 });
